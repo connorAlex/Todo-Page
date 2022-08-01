8da175205b2d7a9a7295623eeb9541ada171029a
connorAlex <alex.connor.cochrane@gmail.com>
2022-08-01 13:01:37 -0500

    added JS skeleton and Task Factory

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
 import './style.css';
+
+// factory functions
+
+// tasks
+const createTask = (name, desc, priority, dueDate) => {
+    
+    const getName = () => name;
+    const getDesc = () => desc;
+    const getPriority = () => priority;
+    const getDueDate = () => dueDate;
+
+    return {getName, getDesc, getPriority, getDueDate};
+};
+
+
+// Projects 
+const createProject = (name) => {
+
+    //init array
+    let projectArray = [];
+
+    const displayTasks = () => {
+        projectArray.forEach((e) => console.log(e));
+    }
+
+    return {displayProject};
+}
+// modules
+
+// displayController
+const displayController = (() => {
+
+})();
+
+// Event Handler
+const eventHandler = (() => {
+
+})();
