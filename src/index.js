e3e93db123c5aa91ade06741aa3d61e33cc37911
connorAlex <alex.connor.cochrane@gmail.com>
2022-08-01 16:06:43 -0500

    added module testing

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './style.css';
-import { Project } from './js/project';
+import { Project, projectHandler } from './js/project';
 import { Task } from './js/task';
 
 const testTask = Task('test_task');
@@ -8,10 +8,21 @@ const testProject = Project('test_project');
 testProject.addTask(testTask);
 testProject.getTasks();
 
+const newproject = Project('myProject');
+projectHandler.addProject(newproject);
+
+const newproject2 = Project('myProject2');
+projectHandler.addProject(newproject2);
+
+projectHandler.logProjects();
+
+projectHandler.removeProject(newproject);
+projectHandler.logProjects();
+
 // displayController
 const displayController = (() => {
     const updateProjects = () => {
-        console.log("updateprojects");
+        console.log("update projects");
     };
 
     const updateTasks = () => {
