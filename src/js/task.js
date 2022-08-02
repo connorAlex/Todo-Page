25fcac40c9435313078b0a51931167350e432bcd
connorAlex <alex.connor.cochrane@gmail.com>
2022-08-01 19:44:43 -0500

    added date conversion

--- src/js/task.js
+++ src/js/task.js
@@ -1,10 +1,21 @@
-// tasks
+import {parse, isPast} from 'date-fns';
+
 const Task = (name, desc, priority, dueDate) => {
+    //convert html dueDate to a date object
+    const date = parse(dueDate, 'yyyy-mm-dd', new Date())
     
     const getName = () => name;
     const getDesc = () => desc;
     const getPriority = () => priority;
-    const getDueDate = () => dueDate;
+    const getDueDate = () => date;
+
+    // checks if dueDate is before current date
+    const isPastDue = () => {
+        if (isPast(date) === true){
+            return true;
+        }
+        return false;
+    };
 
     return {getName, getDesc, getPriority, getDueDate};
 };
