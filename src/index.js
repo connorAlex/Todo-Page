2711032743c76779a5ef132f4d1edf157ef91a59
connorAlex <alex.connor.cochrane@gmail.com>
2022-08-01 19:49:58 -0500

    add updateTask function

--- src/index.js
+++ src/index.js
@@ -8,12 +8,16 @@ const testProject = Project('test_project');
 
 // displayController
 const displayController = (() => {
+
     const updateProjects = () => {
 
         // loop through all projects in the project handler array and display on DOM
         const projects = projectHandler.getProjects();
         const projectContainer = document.querySelector('.projectContainer');
+
+        //reset Project buttons
         projectContainer.innerHTML = "";
+
         projects.forEach((e)=> {
             let btn = document.createElement('button');
             btn.innerHTML = e.getName();
@@ -23,13 +27,29 @@ const displayController = (() => {
 
     const updateTasks = (project) => {
         // loop through the selected project and display all tasks
-        console.log("update Tasks");
+        const tasks = project.getTasks();
+        const taskContainer = document.querySelector('.taskContainer');
+
+        //reset Tasks
+        taskContainer.innerHTML = "";
+
+        // add task cards in
+        tasks.forEach((e)=>{
+            let card = document.createElement('div');
+            card.classList.add('card');
+
+            let cardName = document.createElement('div');
+            cardName.classList.add('title');
+            
+            card.appendChild(cardName);
+            taskContainer.appendChild(card);
+        })
     };
 
-    return { updateProjects, updateTasks};
+    return {updateProjects, updateTasks};
 })();
 
-displayController.updateProjects();
+
 // Event Handler
 const eventHandler = ((event) => {
 
