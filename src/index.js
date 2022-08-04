3deccd229c61be72fa6ded17379bf87b8b1a52c2
connorAlex <alex.connor.cochrane@gmail.com>
2022-08-04 14:44:58 -0500

    prettified doc

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
-import './style.css';
-import { displayController } from './js/display';
-import { Project, projectHandler } from './js/project';
-import { Task } from './js/task';
-import { eventHandler } from './js/event';
+import "./style.css";
+import { displayController } from "./js/display";
+import { Project, projectHandler } from "./js/project";
+import { Task } from "./js/task";
+import { eventHandler } from "./js/event";
 
 const pageLoad = (() => {
-    const addProjectBtn = document.querySelector(".addProjectBtn");
-    const projectInput = document.querySelector(".projectInput");
-    addProjectBtn.addEventListener("click", (e) => eventHandler.insertProject(projectInput));
+  const addProjectBtn = document.querySelector(".addProjectBtn");
+  const projectInput = document.querySelector(".projectInput");
+  addProjectBtn.addEventListener("click", (e) =>
+    eventHandler.insertProject(projectInput)
+  );
 
-    const testTask = Task('test_task');
-    const testProject = Project('test_project');
+  const testTask = Task("test_task");
+  const testProject = Project("test_project");
 
-    testProject.addTask(testTask);
-    projectHandler.addProject(testProject);
+  testProject.addTask(testTask);
+  projectHandler.addProject(testProject);
 
-    displayController.updateProjects();
+  displayController.updateProjects();
 })();
