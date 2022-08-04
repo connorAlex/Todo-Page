74a200a6ba6d4a34f5abdd7540c1ba267a63fc74
connorAlex <alex.connor.cochrane@gmail.com>
2022-08-04 11:09:49 -0500

    added taskname displaying in dom

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import { Project, projectHandler } from './js/project';
 import { Task } from './js/task';
 
 
-const testTask = Task('test_task');
-const testProject = Project('test_project');
+
+
 
 // displayController
 const displayController = (() => {
@@ -21,26 +21,35 @@ const displayController = (() => {
         projects.forEach((e) => {
             let btn = document.createElement('button');
             btn.innerHTML = e.getName();
+
+            btn.classList.add('selectProjectBtn');
+            btn.setAttribute("name", e.getName());
             projectContainer.appendChild(btn);
+            
         });
+        eventHandler.addSelectListeners();
     };
 
     const updateTasks = (project) => {
         // loop through the selected project and display all tasks
-        console.log(project);
+        console.log("update tasks fired");
+
         const tasks = project.getTasks();
         const taskContainer = document.querySelector('.taskContainer');
-
+        console.log(tasks);
         //reset Tasks
         taskContainer.innerHTML = "";
 
         // add task cards in
         if(tasks) {
             tasks.forEach((e) => {
+
+
                 let card = document.createElement('div');
                 card.classList.add('card');
     
                 let cardName = document.createElement('div');
+                cardName.innerHTML = e.getName();
                 cardName.classList.add('title');
                 
                 card.appendChild(cardName);
@@ -61,24 +70,38 @@ const eventHandler = ((event) => {
     let currentProject = "";
 
     const selectProject = (project) => {
-        
+        console.log("selectProject fired");
         currentProject = project;
         const projectObject = projectHandler.findProject(project);
         displayController.updateTasks(projectObject);
     };
     
-    const selectProjectBtns = document.querySelectorAll(".selectProjectBtn");
-    selectProjectBtns.forEach((e) => {
-        e.addEventListener("click",function() {
-            console.log(e.name);
-            selectProject(e.name);
-            
+    const addSelectListeners = () => {
+
+        console.log("addSelectListeners fired");
+
+        const selectProjectBtns = document.querySelectorAll(".selectProjectBtn");
+        selectProjectBtns.forEach((e) => {
+
+            e.addEventListener("click",function() {
+                console.log(e.name);
+                selectProject(e.name);
+            });
         });
-    });
+    }
+
+    
 
 
     const addProjectBtn = document.querySelector(".addProjectBtn");
     addProjectBtn.addEventListener("click", function() {console.log("project click")});
     
+    return {addSelectListeners};
+})();
+const testTask = Task('test_task');
+const testProject = Project('test_project');
 
-})(); 
+testProject.addTask(testTask);
+projectHandler.addProject(testProject);
+
+displayController.updateProjects();
