d7667c308064ce1790f67b4b89d4b7d0ce01d32a
connorAlex <alex.connor.cochrane@gmail.com>
2022-08-17 11:56:57 -0500

    refactored elementCreator

--- src/js/display.js
+++ src/js/display.js
@@ -1,6 +1,7 @@
 import { Project, projectHandler } from "./project";
 import { eventHandler } from "./event";
 import { Task } from "./task";
+import { elementCreator } from "./elementCreator";
 
 const displayController = (() => {
   const projectContainer = document.querySelector(".projectContainer");
@@ -20,7 +21,6 @@ const displayController = (() => {
 
     taskContainer.innerHTML = "";
     if (tasks) tasks.forEach((e) => elementCreator.createCard(e));
-   
   };
 
   const clearInputs = () => {
@@ -52,123 +52,6 @@ const displayController = (() => {
   return { updateProjects, updateTasks, clearInputs, addSelectedBtnStyle };
 })();
 
-const elementCreator = (() => {
-  const projectContainer = document.querySelector(".projectContainer");
-  const taskContainer = document.querySelector(".taskContainer");
-
-  const createProjectButton = (project) => {
-    let btn = document.createElement("button");
-    let projectName = project.getName();
-
-    btn.innerHTML = projectName;
-    btn.setAttribute("name", projectName);
-    btn.classList.add("selectProjectBtn");
-    projectContainer.appendChild(btn);
-  };
-
-  //create element
-  const crel = (type, classAdd) => {
-    let output = document.createElement(type);
-    output.classList.add(classAdd);
-
-    return output;
-  };
-
-  const createCheck = (task) => {
-    let check = document.createElement("input");
-    // adding the "completed" styling
-    if (task.getStatus() === true) {
-      title.classList.add("complete");
-      check.checked = true;
-    };
-    check.type = "checkbox";
-    check.name = task.getName();
-
-    check.addEventListener("click", function(e) {
-      e.stopPropagation();
-      strikeTask(check)
-    });
-    return check;
-  };
-
-  const createDeleteBtn = (name) => {
-    let deleteBtn = crel("button","deleteBtn");
-    deleteBtn.name = name;
-    deleteBtn.innerHTML = "x";
-
-    deleteBtn.addEventListener("click", function(e) {
-      e.stopPropagation();
-      eventHandler.deleteTask(deleteBtn);
-      
-    });
-
-    return deleteBtn;
-  };
-  
-  const createTitle = (name) => {
-    let title = document.createElement("p");
-    title.innerHTML += name;
-
-    return title;
-  };
-
-  const createCardInfo = (task) => {
-    let content = crel("div","content");
-    let deleteBtn = createDeleteBtn(task.getName())
-    let check = createCheck(task);
-    
-    let subject = crel("div","subject");
-
-    let children = [
-      createCheck(task),
-      createTitle(task.getName()),
-      createDeleteBtn(task.getName())
-    ];
-
-    children.forEach((e) => subject.appendChild(e));
-
-    content.innerHTML += task.getDesc();
-
-    return {subject,content}
-  };
-   
-  const createCard = (task) => {
-    let card = crel("div","taskCard");
-    let info = createCardInfo(task);
-    
-    console.log(info.subject);
-
-    card.appendChild(info.subject);
-    card.appendChild(info.content);
-    taskContainer.appendChild(card);
-    animateTask(card);
-
-  };
-
-  const animateTask = (element) => {
-
-    element.addEventListener("click", (e) => {
-      element.classList.toggle("active");
-      let content = element.children[1];
-      if (content.style.maxHeight) {
-        content.style.maxHeight = null;
-      } else {
-        content.style.maxHeight = content.scrollHeight + "px";
-      }
-    });
-  };
-
-  
-
-  const strikeTask = (checkbox) => {
-    eventHandler.completeTask(checkbox);
-    checkbox.parentElement.classList.toggle("complete");
-  };
-
-
-  return { createProjectButton, createCard };
-})();
-
 const overlayController = (() => {
   const overlay = document.querySelector("#taskInputOverlay");
 
@@ -191,7 +74,6 @@ const overlayController = (() => {
   };
 
   const submitTask = () => {
-
     let currentProjectName = eventHandler.getCurrentProject();
     let currentProject = projectHandler.findProject(currentProjectName);
     let newTask = getInputs();
