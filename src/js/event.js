697917e5562c4ed070574eb96aebb91a742c15d8
connorAlex <alex.connor.cochrane@gmail.com>
2022-08-19 15:38:01 -0500

    linting adjustments

--- src/js/event.js
+++ src/js/event.js
@@ -1,48 +1,52 @@
-import { Project, projectHandler } from './project';
-import { Task } from './task';
-import { displayController } from './display';
+import { Project, projectHandler } from "./project";
+import { displayController } from "./display";
 
 const eventHandler = (() => {
+  let currentProject = "";
 
-    let currentProject = "";
+  //change this to (projectHandler.findProject(currentProject);)?
+  const getCurrentProject = () => currentProject;
 
-    //change this to (projectHandler.findProject(currentProject);)?
-    const getCurrentProject = () => currentProject;
+  const selectProject = (project) => {
+    currentProject = project;
+    const projectObject = projectHandler.findProject(project);
+    if (projectObject) displayController.updateTasks(projectObject);
+  };
 
-    const selectProject = (project) => {
-        currentProject = project;
-        const projectObject = projectHandler.findProject(project);
-        if (projectObject) displayController.updateTasks(projectObject);
-    };
-    
-    const insertProject = (element) => {
-        if (element.value === '' || !element.value.match(/^[0-9A-Za-z]+$/)) {return false};
+  const insertProject = (element) => {
+    if (element.value === "" || !element.value.match(/^[0-9A-Za-z]+$/)) {
+      return false;
+    }
 
-        let newProject = Project(element.value);
-        projectHandler.addProject(newProject);
-        selectProject(newProject.getName());
+    let newProject = Project(element.value);
+    projectHandler.addProject(newProject);
+    selectProject(newProject.getName());
 
-        displayController.updateProjects();
-        displayController.clearInputs();
-        
-    };
+    displayController.updateProjects();
+    displayController.clearInputs();
+  };
 
-    const deleteTask = (element) => {
-        let project = projectHandler.findProject(currentProject);
-        let task = project.findTask(element.name);
-        project.removeTask(element.name);
-        displayController.updateTasks(project);
-        projectHandler.storeProjects(project);
-    };
+  const deleteTask = (element) => {
+    let project = projectHandler.findProject(currentProject);
+    project.removeTask(element.name);
+    displayController.updateTasks(project);
+    projectHandler.storeProjects(project);
+  };
 
-    const completeTask = (element) => {
-        let project = projectHandler.findProject(currentProject);
-        let task = project.findTask(element.name);
-        task.toggleStatus();
-        projectHandler.storeProjects(project);
-    };
+  const completeTask = (element) => {
+    let project = projectHandler.findProject(currentProject);
+    let task = project.findTask(element.name);
+    task.toggleStatus();
+    projectHandler.storeProjects(project);
+  };
 
-    return {getCurrentProject, selectProject, insertProject, completeTask, deleteTask};
+  return {
+    getCurrentProject,
+    selectProject,
+    insertProject,
+    completeTask,
+    deleteTask,
+  };
 })();
 
-export {eventHandler};
+export { eventHandler };
