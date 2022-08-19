9e52b3f4c802f1f46b63095db842244ebe79607f
connorAlex <alex.connor.cochrane@gmail.com>
2022-08-19 15:34:20 -0500

    linting changes

--- src/index.js
+++ src/index.js
@@ -1,29 +1,16 @@
 import "./style.css";
 import { displayController } from "./js/display";
-import { Project, projectHandler } from "./js/project";
-import { Task } from "./js/task";
+import { projectHandler } from "./js/project";
 import { eventHandler } from "./js/event";
 import { overlayController } from "./js/overlayController";
 
 const pageLoad = (() => {
   const addProjectBtn = document.querySelector(".addProjectBtn");
   const projectInput = document.querySelector(".projectInput");
-  
 
-  addProjectBtn.addEventListener("click", (e) => {
+  addProjectBtn.addEventListener("click", () => {
     eventHandler.insertProject(projectInput);
   });
-
-  // const loadDefault = (() => {
-  //   const testTask = Task("Clean my room", "Clean that shit up", true, "2022-03-15");
-  //   const testProject = Project("myProject");
-
-  //   testProject.addTask(testTask);
-  //   projectHandler.addProject(testProject);
-    
-  //   eventHandler.selectProject(testProject.getName());
-  //   //projectHandler.storeProjects(testProject);
-  // })();
 
   overlayController.createOverlay();
   projectHandler.returnSavedProjects();
