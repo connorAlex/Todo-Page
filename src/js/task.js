246f3707f83fd8dfdfe8afbae796e864501d140c
connorAlex <alex.connor.cochrane@gmail.com>
2022-08-19 15:41:35 -0500

    linter adjustments

--- src/js/task.js
+++ src/js/task.js
@@ -1,33 +1,38 @@
-import {format, parse, isPast} from 'date-fns';
+import { format, parse, isPast } from "date-fns";
 
-//use a default variable to set the status to false, then you can save the status
 const Task = (name, desc, priority, dueDate, newstatus = false) => {
-    
-    let status = newstatus;
-    // let status = false;
-    //convert html dueDate to a date object
-    const date = format(parse(dueDate, 'yyyy-mm-dd', new Date()),"MM/dd/yyyy");
-    
-    const getName = () => name;
-    const getDesc = () => desc;
-    const getPriority = () => priority;
-    const getDueDate = () => dueDate;
-    const getStatus = () => status;
-    const toggleStatus = () =>  {
-        status = !status;
-        return status
+  let status = newstatus;
+
+  //convert html dueDate to a date object
+  const date = format(parse(dueDate, "yyyy-mm-dd", new Date()), "MM/dd/yyyy");
+
+  const getName = () => name;
+  const getDesc = () => desc;
+  const getPriority = () => priority;
+  const getDueDate = () => dueDate;
+  const getStatus = () => status;
+  const toggleStatus = () => {
+    status = !status;
+    return status;
+  };
+
+  // checks if dueDate is before current date
+  const isPastDue = () => {
+    if (isPast(date) === true) {
+      return true;
     }
+    return false;
+  };
 
-
-    // checks if dueDate is before current date
-    // const isPastDue = () => {
-    //     if (isPast(date) === true){
-    //         return true;
-    //     }
-    //     return false;
-    // };
-
-    return {getName, getDesc, getPriority, getDueDate, toggleStatus, getStatus};
+  return {
+    getName,
+    getDesc,
+    getPriority,
+    getDueDate,
+    toggleStatus,
+    isPastDue,
+    getStatus,
+  };
 };
 
-export {Task};
+export { Task };
