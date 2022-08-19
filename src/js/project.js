d280e429d0cfdefdd6a37e1e440895d3024b338c
connorAlex <alex.connor.cochrane@gmail.com>
2022-08-19 15:39:32 -0500

    prettier adjustments

--- src/js/project.js
+++ src/js/project.js
@@ -1,106 +1,108 @@
 import { Task } from "./task";
 
 const Project = (name) => {
+  let projectArray = [];
 
-    let projectArray = [];
+  const getName = () => name;
 
-    const getName = () => name;
+  const getTasks = () => projectArray;
 
-    const getTasks = () => projectArray;
+  const addTask = (task) => {
+    projectArray.push(task);
+  };
 
-    const addTask = (task) => {
-        projectArray.push(task);
-    };
+  const getTaskIndex = (nameString) => {
+    let obj = projectArray.find((e) => e.getName() === nameString);
+    let index = projectArray.indexOf(obj);
+    return index;
+  };
 
-    const getTaskIndex = (nameString) => {
-        let obj = projectArray.find(e => e.getName() === nameString);
-        let index = projectArray.indexOf(obj);
-        return index;
-    };
+  const findTask = (nameString) => {
+    let obj = projectArray.find((e) => e.getName() === nameString);
+    return obj;
+  };
 
-    const findTask = (nameString) => {
-        let obj = projectArray.find(e => e.getName() === nameString);
-        return obj;
-    };
+  const removeTask = (task) => {
+    let index = getTaskIndex(task);
+    projectArray.splice(index, 1);
+  };
 
-    const removeTask = (task) => {
-        let index = getTaskIndex(task);
-        projectArray.splice(index, 1);
-    };
+  const saveTasks = () => {
+    let taskJson = [];
+    projectArray.forEach((e) => {
+      let taskObject = {
+        name: e.getName(),
+        desc: e.getDesc(),
+        priority: e.getPriority(),
+        dueDate: e.getDueDate(),
+        status: e.getStatus(),
+      };
+      taskJson.push(taskObject);
+    });
 
-    const saveTasks = () => {
-        let taskJson = [];
-        projectArray.forEach((e) => {
-            let taskObject = {
-                name: e.getName(),
-                desc: e.getDesc(),
-                priority: e.getPriority(),
-                dueDate: e.getDueDate(),
-                status: e.getStatus()
-            };
-            taskJson.push(taskObject);
-        });
-        
-        return taskJson;
-    };
+    return taskJson;
+  };
 
-
-    return {addTask, getTasks, removeTask, getName, findTask, saveTasks};
-}
+  return { addTask, getTasks, removeTask, getName, findTask, saveTasks };
+};
 
 const projectHandler = (() => {
-    
-    let projectList = [];
-    
-    const addProject = (project) => {
-        projectList.push(project);
-        storeProjects(project);
+  let projectList = [];
+
+  const addProject = (project) => {
+    projectList.push(project);
+    storeProjects(project);
+  };
+
+  const removeProject = (project) => {
+    const index = projectList.indexOf(project);
+    projectList.splice(index, 1);
+  };
+
+  const getProjects = () => {
+    return projectList;
+  };
+
+  const findProject = (projectName) => {
+    let obj = projectList.find((e) => e.getName() === projectName);
+    return obj;
+  };
+
+  const saveProject = (project) => {
+    let projectObject = {
+      name: project.getName(),
+      tasks: project.saveTasks(),
     };
+    return projectObject;
+  };
 
-    const removeProject = (project) => {
-        const index = projectList.indexOf(project);
-        projectList.splice(index, 1);
-    };
+  const storeProjects = (project) => {
+    let projectObject = saveProject(project);
+    localStorage.setItem(projectObject.name, JSON.stringify(projectObject));
+  };
 
-    const getProjects = () => {
-        return projectList;
-    };
+  const returnSavedProjects = () => {
+    for (let i = 0; i < localStorage.length; i++) {
+      let projectObject = JSON.parse(localStorage.getItem(localStorage.key(i)));
+      let newProject = Project(projectObject.name);
 
-    const findProject = (projectName) => {
-        let obj = projectList.find((e) => e.getName() === projectName);
-        return obj;
-    };
+      projectObject.tasks.forEach((e) => {
+        let newTask = Task(e.name, e.desc, e.priority, e.dueDate, e.status);
+        newProject.addTask(newTask);
+      });
 
-    const saveProject = (project) => {
-        let projectObject = {
-            name: project.getName(),
-            tasks: project.saveTasks()
-        };
-        return projectObject;
-    };
-    
-    const storeProjects = (project) => {
-        let projectObject = saveProject(project);
-        localStorage.setItem(projectObject.name, JSON.stringify(projectObject));
-    };
+      addProject(newProject);
+    }
+  };
 
-
-    const returnSavedProjects = () => {
-        for (let i = 0; i < localStorage.length; i++){
-            let projectObject = JSON.parse(localStorage.getItem(localStorage.key(i)));
-            let newProject = Project(projectObject.name);
-
-            projectObject.tasks.forEach((e) => {
-                let newTask = Task(e.name,e.desc,e.priority, e.dueDate, e.status);
-                newProject.addTask(newTask);
-            });
-
-            addProject(newProject);
-
-        }
-    };
-
-    return {addProject, removeProject, getProjects, findProject, storeProjects, returnSavedProjects};
+  return {
+    addProject,
+    removeProject,
+    getProjects,
+    findProject,
+    storeProjects,
+    returnSavedProjects,
+  };
 })();
 
-export {Project, projectHandler};
+export { Project, projectHandler };
