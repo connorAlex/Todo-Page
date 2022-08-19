1929e241bd7f8fe3019b2344f71dd53e33eb80d3
connorAlex <alex.connor.cochrane@gmail.com>
2022-08-19 15:38:41 -0500

    prettier changes

--- src/js/overlayController.js
+++ src/js/overlayController.js
@@ -4,63 +4,62 @@ import { eventHandler } from "./event";
 import { projectHandler } from "./project";
 
 const overlayController = (() => {
-    const overlay = document.querySelector("#taskInputOverlay");
-  
-    const getInputs = () => {
-      let inputs = document.querySelectorAll(".taskInput");
-      
-      if (!formVerification(inputs)) return false;
-      let newTask = Task(
-        inputs[0].value,
-        inputs[3].value,
-        inputs[2].checked,
-        inputs[1].value
-      );
-      return newTask;
-    };
-  
-    const submitTask = () => {
-      let currentProjectName = eventHandler.getCurrentProject();
-      
-      let currentProject = projectHandler.findProject(currentProjectName);
-      if(currentProject) {
-        let newTask = getInputs();
-  
-        currentProject.addTask(newTask);
-        displayController.clearInputs();
-        displayController.updateTasks(currentProject);
+  const overlay = document.querySelector("#taskInputOverlay");
+
+  const getInputs = () => {
+    let inputs = document.querySelectorAll(".taskInput");
+
+    if (!formVerification(inputs)) return false;
+    let newTask = Task(
+      inputs[0].value,
+      inputs[3].value,
+      inputs[2].checked,
+      inputs[1].value
+    );
+    return newTask;
+  };
+
+  const submitTask = () => {
+    let currentProjectName = eventHandler.getCurrentProject();
+
+    let currentProject = projectHandler.findProject(currentProjectName);
+    if (currentProject) {
+      let newTask = getInputs();
+
+      currentProject.addTask(newTask);
+      displayController.clearInputs();
+      displayController.updateTasks(currentProject);
+      overlay.classList.toggle("toggle");
+      projectHandler.storeProjects(currentProject);
+    }
+  };
+
+  const formVerification = (inputArr) => {
+    let output = true;
+    for (let i = 0; i < inputArr.length; i++) {
+      if (inputArr[i].value === "" && inputArr[i].type != "checkbox") {
+        output = false;
+      }
+    }
+
+    return output;
+  };
+
+  const createOverlay = () => {
+    let addBtn = document.querySelector(".taskBtn");
+    let submitBtn = document.querySelector(".submit");
+    let exitBtn = document.querySelector(".overlayExit");
+
+    exitBtn.addEventListener("click", () => overlay.classList.toggle("toggle"));
+    addBtn.addEventListener("click", () => {
+      if (eventHandler.getCurrentProject() != "") {
         overlay.classList.toggle("toggle");
-        projectHandler.storeProjects(currentProject);
       }
-      
-    };
-  
-    const formVerification = (inputArr) => {
-      let output = true;
-      for (let i = 0; i < inputArr.length; i++) {
-        if (inputArr[i].value === "" && inputArr[i].type != "checkbox") {
-          output = false;
-        }
-      }
+    });
+    submitBtn.addEventListener("click", () => submitTask());
+  };
 
-      return output;
-    };
-  
-    const createOverlay = () => {
-      let addBtn = document.querySelector(".taskBtn");
-      let submitBtn = document.querySelector(".submit");
-      let exitBtn = document.querySelector(".overlayExit");
+  return { createOverlay, submitTask };
+})();
 
-      exitBtn.addEventListener("click", () => overlay.classList.toggle("toggle"));
-      addBtn.addEventListener("click", () => {
-        if (eventHandler.getCurrentProject() != ""){
-          overlay.classList.toggle("toggle");
-        }
-      });
-      submitBtn.addEventListener("click", () => submitTask());
-    };
-  
-    return { createOverlay, submitTask };
-  })();
-
-  export {overlayController};
+export { overlayController };
